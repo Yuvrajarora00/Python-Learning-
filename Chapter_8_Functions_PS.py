f69d9436d6039d281cfd47497c29f2287dae34d6
Yuvarjarora00 <yuvrajarora0090@example.com>
2026-02-24 23:19:13 +0530

    Add snake water gun game

--- Chapter_8_Functions_PS.py
+++ Chapter_8_Functions_PS.py
@@ -1,57 +1,57 @@
-# # problem 1 to find the greates number 
+# problem 1 to find the greates number 
 
-# def greatest(a ,b ,c):
-#     if a > b and a > c:
-#         return a
-#     elif b > c and b > a:
-#         return b
-#     else:
-#         return c
+def greatest(a ,b ,c):
+    if a > b and a > c:
+        return a
+    elif b > c and b > a:
+        return b
+    else:
+        return c
         
 
-# a = int(input("enter a number: "))
-# b = int(input("enter a number: "))
-# c = int(input("enter a number: "))
-# print(greatest(a,b,c))
+a = int(input("enter a number: "))
+b = int(input("enter a number: "))
+c = int(input("enter a number: "))
+print(greatest(a,b,c))
 
 
-# # problem 2 celcius to fehranite
+# problem 2 celcius to fehranite
 
-# def new_func(k):
-#     return 5 * (k-32)/9
+def new_func(k):
+    return 5 * (k-32)/9
 
-# k = float(input("enter a number to f: "))
-# c = new_func(k)
-# print(f"{round(c, 2)} degree celcius")
+k = float(input("enter a number to f: "))
+c = new_func(k)
+print(f"{round(c, 2)} degree celcius")
 
 
 # problem 3
 
-# def print_sum(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         return print_sum(n-1) + n
+def print_sum(n):
+    if n == 0:
+        return 0
+    else:
+        return print_sum(n-1) + n
     
-# print(print_sum(5))
+print(print_sum(5))
 
 
 # problem 4
-# def line(k):
-#     if(k == 0):
-#         return 0
-#     print("*" * k)
-#     line(k-1)
-#     print()
+def line(k):
+    if(k == 0):
+        return 0
+    print("*" * k)
+    line(k-1)
+    print()
         
-# line(3)
+line(3)
 
 # problem 5
-# def inc_cm(inc):
-#     return inc * 2.54
+def inc_cm(inc):
+    return inc * 2.54
     
-# k = int(input("enter a number to covert inc to cm: "))
-# print(inc_cm(k))
+k = int(input("enter a number to covert inc to cm: "))
+print(inc_cm(k))
 
 # problem 6
 def table(n):
---
+++ Chpater_9_I_O.py
@@ -0,0 +1,37 @@
+# this will read and print the whole file 
+
+# f = open("poem.txt","r")
+# data = f.read()
+# print(data)
+# f.close()
+
+# # and if u want to read only few characters use this this includes space also as characters
+
+# a = open("poem.txt","r")
+# data1 = a.readline(5)
+# print(data1)
+# a.close()
+
+# # if u want to write in the file this method write in your file but this will delete all your old data u write in the file.
+
+# b = open("poem2.txt","w")
+# b.write("heloooo")
+# b.close()
+
+
+# # this will create a new file and open it for writing
+
+# c = open("poem3.txt","x")
+# c.write("hello world!")
+# c.close()
+
+# with this we will write our content at the end of the file 
+
+# d = open("poem.txt","a")
+# d.write("hello world i am yuvi")
+# d.close()
+
+e = open("poem.txt","rb")
+data = e.read()
+print(data)
+e.close()
