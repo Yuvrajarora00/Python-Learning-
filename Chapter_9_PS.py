34960a0600a526394bc81aee5226cf21f76717dc
Yuvarjarora00 <yuvrajarora0090@example.com>
2026-03-14 01:32:52 +0530

    Updated Chapter 9 IO code and added new practice files

--- Chapter_9_PS.py
+++ Chapter_9_PS.py
@@ -1,101 +1,124 @@
-# # problem 1
+# problem 1
 
-# # f = open("poem2.txt","r")
-# # c = f.read()
-# # if ("heektkl" in c):
-# #     print("yes there is word twinkle")
-# # else:
-# #     print("no there is no word like this")
-# # f.close
+# f = open("poem2.txt", "r")
+# c = f.read()
+# if "heektkl" in c:
+#     print("yes there is word twinkle")
+# else:
+#     print("no there is no word like this")
+# f.close
 
 
-# # # problem 2
-# # import random
+# # problem 2
+# import random
 
-# # def game():
-# #     print("you are playing a game")
-# #     score = random.randint(1,100)
-    
-# #     # fetch the hiscore
-# #     with open("hiscore.txt") as f: 
-# #         hiscore = f.read()
-# #         if (hiscore != ""):
-# #             hiscore = int(hiscore)
-# #         else:
-# #             hiscore = 0
-            
-# #     print(f"your score: {score}")
-# #     if score>hiscore:
-# #         with open("hiscore.txt","w") as f:
-# #             f.write(str(score))
-            
-# #         return score
-    
-    
-# # game()
+
+# def game():
+#     print("you are playing a game")
+#     score = random.randint(1, 100)
+
+#     # fetch the hiscore
+#     with open("hiscore.txt") as f:
+#         hiscore = f.read()
+#         if hiscore != "":
+#             hiscore = int(hiscore)
+#         else:
+#             hiscore = 0
+
+#     print(f"your score: {score}")
+#     if score > hiscore:
+#         with open("hiscore.txt", "w") as f:
+#             f.write(str(score))
+
+#         return score
+
+
+# game()
 
 # # problem 3
 
-# # def table(n):
-# #     table = ""
-# #     for i in range(1,11):
-# #         table += f"{n} X {i} = {n*i}\n"
-        
-# #     with open(f"tables/table_{n}.txt","w") as f:
-# #         f.write(table)
 
- 
-# # for i in range(2,21):
-# #     table(i)
-    
-    
+# def table(n):
+#     table = ""
+#     for i in range(1, 11):
+#         table += f"{n} X {i} = {n*i}\n"
+
+#     with open(f"tables/table_{n}.txt", "w") as f:
+#         f.write(table)
+
+
+# for i in range(2, 21):
+#     table(i)
+
+
 # # problem 4
 
 # word = "donkey"
 
-# with open("donkey.txt","r") as f:
-#     content = f.read() 
-    
-# newcontent = content.replace(word,"#####")
+# with open("donkey.txt", "r") as f:
+#     content = f.read()
 
-# with open("donkey.txt","w") as f:    
+# newcontent = content.replace(word, "#####")
+
+# with open("donkey.txt", "w") as f:
 #     f.write(newcontent)
-    
-    
-    
+
+
 # # problem 5
 # word = "donkey"
 
-# with open("donkey.txt","r") as f:
-#     content = f.read() 
-    
-# newcontent = content.replace(word,"@"*len(word))
-
-# with open("donkey.txt","w") as f:    
-#     f.write(newcontent)
-    
-    
-    
-# # problem 6
-# with open("python.txt","r") as f:
+# with open("donkey.txt", "r") as f:
 #     content = f.read()
-#     if ("python" in content):
+
+# newcontent = content.replace(word, "@" * len(word))
+
+# with open("donkey.txt", "w") as f:
+#     f.write(newcontent)
+
+
+# # problem 6
+# with open("python.txt", "r") as f:
+#     content = f.read()
+#     if "python" in content:
 #         print("there is word python")
 #     else:
 #         print("there is no word like that")
 
 
-# problem 6
+# # problem 6
 
 
-with open("pyec.txt") as f:
-    lines = f.readlines()
+# with open("pyec.txt") as f:
+#     lines = f.readlines()
 
-lineno = 1
-for line in lines:
-    if "python" in line:
-        print(f"python word is present in line number: {lineno}")
-        break
-    lineno += 1
-else:
-    print("there is no word like that")
+# lineno = 1
+# for line in lines:
+#     if "python" in line:
+#         print(f"python word is present in line number: {lineno}")
+#         break
+#     lineno += 1
+# else:
+#     print("there is no word like that")
+
+
+# problem 7
+
+# with open("pyth.txt") as f:
+#     lines = f.readlines()
+
+# lineno = 1
+# for line in lines:
+#     if "python" in line:
+#         print(f"yes in line no {lineno}")
+#         break
+#     lineno += 1
+# else:
+#     print("no")
+
+
+# program 8
+with open("copy.txt", "r") as f:
+    content = f.read()
+
+with open("copy_1.txt", "w") as f:
+    f.write(content)
--- Chpater_9_I_O.py
+++ Chpater_9_I_O.py
@@ -1,43 +1,43 @@
 # this will read and print the whole file 
 
-# f = open("poem.txt","r")
-# data = f.read()
-# print(data)
-# f.close()
+f = open("poem.txt","r")
+data = f.read()
+print(data)
+f.close()
 
-# # and if u want to read only few characters use this this includes space also as characters
+# and if u want to read only few characters use this this includes space also as characters
 
-# a = open("poem.txt","r")
-# data1 = a.readline(5)
-# print(data1)
-# a.close()
+a = open("poem.txt","r")
+data1 = a.readline(5)
+print(data1)
+a.close()
 
 # # if u want to write in the file this method write in your file but this will delete all your old data u write in the file.
 
-# b = open("poem2.txt","w")
-# b.write("heloooo")
-# b.close()
+b = open("poem2.txt","w")
+b.write("heloooo")
+b.close()
 
 
-# # this will create a new file and open it for writing
+# this will create a new file and open it for writing
 
-# c = open("poem3.txt","x")
-# c.write("hello world!")
-# c.close()
+c = open("poem3.txt","x")
+c.write("hello world!")
+c.close()
 
 # with this we will write our content at the end of the file 
 
-# d = open("poem.txt","a")
-# d.write("hello world i am yuvi")
-# d.close()
+d = open("poem.txt","a")
+d.write("hello world i am yuvi")
+d.close()
 
 
 # with this we csan read and write in the file in binary mode
 
-# e = open("poem.txt","rb")
-# data = e.read()
-# print(data)
-# e.close()
+e = open("poem.txt","rb")
+data = e.read()
+print(data)
+e.close()
 
 
 # USE OF + MODE WITH ALL
